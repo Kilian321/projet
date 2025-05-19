1304d3c1591af3fab6a47eeb57adaa1ef002c194
dalexis <108296@ecole-it.com>
2025-05-19 09:31:49 +0200

    ajout de l'icone

---
+++ PHP/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Project</title>
+    <link href="../css/home.css" rel="stylesheet">
+    <link rel="icon" href="../PICTURE/Archeo-IT_Logo-removebg-preview.png">
+</head>
+<body>
+<?php
+echo "hello world";
+?>
+</body>
