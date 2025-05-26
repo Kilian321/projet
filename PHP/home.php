928411e3562ce0f10af293600e59023616919a15
dalexis <108296@ecole-it.com>
2025-05-26 16:43:32 +0200

    commit fin jour 1

--- PHP/home.php
+++ PHP/home.php
@@ -1,1 +1,10 @@
 <?php require_once "navbar.php"; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Archéo-IT Home</title>
+    <meta charset="UTF-8">
+    <link rel="icon" href="../PICTURE/Archeo-IT_Logo-removebg-preview.png">
+</head>
+</html>
+
--- PHP/ourDiscoveries.php
+++ PHP/ourDiscoveries.php
@@ -1,1 +1,9 @@
 <?php require_once "navbar.php"; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Archéo-IT Discoveries</title>
+    <meta charset="UTF-8">
+    <link rel="icon" href="../PICTURE/Archeo-IT_Logo-removebg-preview.png">
+</head>
+</html>
