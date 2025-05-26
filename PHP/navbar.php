c1ff5c842d6272eda592cd000d6d18a43b19e7e3
dalexis <108296@ecole-it.com>
2025-05-26 11:08:23 +0200

    navbar update

--- PHP/navbar.php
+++ PHP/navbar.php
@@ -4,7 +4,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <meta charset="UTF-8">
     <title>Project</title>
-    <link href="../css/navbar.css" rel="stylesheet">
+    <link href="../CSS/navbar.css" rel="stylesheet">
     <link rel="icon" href="../PICTURE/Archeo-IT_Logo-removebg-preview.png">
 </head>
 <body>
