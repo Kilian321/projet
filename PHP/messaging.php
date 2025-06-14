49725c3a7f3b3ba1092cab5126be901f91bf82fb
kilian <kvanovertvelt@gmail.com>
2025-06-14 14:33:23 +0200

    maj format date page messagerie

--- PHP/messaging.php
+++ PHP/messaging.php
@@ -5,6 +5,7 @@ require_once '../DATA-BASE/database.php'; global $pdo;
 
 $sql = "SELECT last_name, first_name, email, menu, message, created_at FROM forms ORDER BY created_at DESC";
 $stmt = $pdo->query($sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -16,21 +17,21 @@ $stmt = $pdo->query($sql);
         <link href="../CSS/messaging.css" rel="stylesheet">
     </head>
 
-    <?php if(!empty($_SESSION['user'])): ?>
-        <h2>Bonjour <?= $_SESSION['user']['email']; ?></h2>
-    <?php endif; ?>
     <div class="chat-container">
         <h2>Messagerie :</h2>
         <div class="chat-box">
-            <?php while ($row = $stmt->fetch()) { ?>
+            <?php while ($row = $stmt->fetch()) {
+                $date = new DateTime($row['created_at']);
+                $date = $date->format('d/m/Y H:i');
+                ?>
                 <div class="message">
                     <div>
-                    <strong><?= htmlspecialchars($row['email']); ?></strong>
+                        <strong><?= htmlspecialchars($row['email']); ?></strong>
                     </div>
-                        <div>
-                    <?= htmlspecialchars($row['last_name']) ?> <?= htmlspecialchars($row['first_name']) ?>
+                    <div>
+                        <?= htmlspecialchars($row['last_name']) ?> <?= htmlspecialchars($row['first_name']) ?>
                     </div>
-                    <span class="date"><?= htmlspecialchars($row['created_at']) ?></span>
+                    <span class="date"><?=$date?></span>
                     <p class="messageText"><?= nl2br(htmlspecialchars($row['message'])) ?></p>
                 </div>
             <?php } ?>
